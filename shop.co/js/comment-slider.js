44770444e7d23543a429848ba67fca873abdeeec
NaNinjia <v22417787@gmail.com>
2026-03-18 16:36:47 +0300

    Add files via upload

---
+++ shop.co/js/comment-slider.js
@@ -0,0 +1,7 @@
+export const commentSlider = () => {
+    new Swiper('.comment__slider', {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        loop: true,
+    })
+}
---
+++ shop.co/js/index.js
@@ -0,0 +1,5 @@
+import { commentSlider } from "./comment-slider";
+
+try {
+    commentSlider();
+};
